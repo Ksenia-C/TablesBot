248574daae5d1506806757f7bd832e957f80ef5c
Ksenia Petrenko <kseniapetrenko6@gmail.com>
2021-12-21 23:28:48 +0300

    ci

---
+++ bot/echo_bot.py
@@ -0,0 +1,20 @@
+"""
+Implementation of echo bot.
+"""
+
+import os
+import telebot
+
+bot = telebot.TeleBot(os.environ.get("TOKEN"), parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    """Send info about bot to user."""
+    bot.reply_to(message, "I will echo all your messages")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    """Echo message."""
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
