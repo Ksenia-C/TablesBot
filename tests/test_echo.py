2e5b214155472495af4826063229fc1096ed56d9
Ksenia Petrenko <kseniapetrenko6@gmail.com>
2021-12-22 00:07:14 +0300

    must docker fail

---
+++ tests/test_echo.py
@@ -0,0 +1,19 @@
+"""
+Unit tests. Right now use only to test github actions.
+"""
+
+import sys
+import unittest
+import logging
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+class TestEchoBot(unittest.TestCase):
+    """Tests for echo bot."""
+
+    def test_nothing(self):
+        """Log message."""
+        log = logging.getLogger("TestEchoBot.test_nothing")
+        log.info(" Has nothing to test but github actions")
+        self.assertEqual(1, 1)
+        
