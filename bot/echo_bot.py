5b60ea8451c5542d3f766954d27cdd8ea7a0d740
Ksenia Petrenko <kseniapetrenko6@gmail.com>
2021-12-22 14:32:56 +0300

    new message

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(os.environ.get("TOKEN"), parse_mode=None)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """Send info about bot to user."""
-    bot.reply_to(message, "I will echo all your messages")
+    bot.reply_to(message, "I will echo all your messages except help and start")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
