3ec183776a5642cb77e2579fbf95e23b46a8b5e2
Ksenia Petrenko <kseniapetrenko6@gmail.com>
2022-01-01 14:48:11 +1000

    rewriting

--- bot/impl_bot.py
+++ bot/impl_bot.py
@@ -5,6 +5,21 @@ Implementation of echo bot.
 import os
 import telebot
 import pandas
+import json
+
+used_commands = {}
+
+
+class Table:
+    def __init__(self,
+                 name=None,
+                 link=None,
+                 export_link=None,
+                 searching_cell=None) -> None:
+        self.name = name
+        self.link = link
+        self.export_link = export_link
+        self.searching_cell = searching_cell
 
 
 def get_export_url(url):
@@ -25,8 +40,8 @@ def get_export_url(url):
             break
     if sheet_id is None:
         return (0, "Failed to identify sheet")
-    return (1, ("https://docs.google.com/spreadsheets/d/",
-                f"{table_id}/export?format=csv&gid={sheet_id}"))
+    return (1, "https://docs.google.com/spreadsheets/d/" +
+            f"{table_id}/export?format=csv&gid={sheet_id}")
 
 
 def make_file_storage_path(chat_id):
@@ -39,10 +54,11 @@ def get_table_from_pandas(url):
     return pandas.read_csv(url)
 
 
-def insert_new_string(chat_id, url, person_name):
+def insert_new_string(chat_id, user_table):
     """Insert table from /add."""
     with open(make_file_storage_path(chat_id), 'a+', encoding='utf-8') as file:
-        file.write(f"{url} {person_name}\n")
+        table_line = json.dumps(user_table.__dict__)
+        file.write(f"{table_line}\n")
 
 
 def get_searching_requests(chat_id):
@@ -52,12 +68,12 @@ def get_searching_requests(chat_id):
         file = open(f"./volumes/{chat_id}.txt", 'r', encoding='utf-8')
     except FileNotFoundError:
         return None
-    request = file.readline()
+    user_table = file.readline()
     result = []
-    while request:
-        space_ind = request.find(' ')
-        result.append((request[: space_ind], request[space_ind + 2: -2]))
-        request = file.readline()
+    while user_table:
+        table = Table(**json.loads(user_table))
+        result.append(table)
+        user_table = file.readline()
     file.close()
     return result
 
@@ -74,41 +90,75 @@ def send_welcome(message):
     /show - show all new changes in tables\n\
     /clear - delete all entries''')
 
+# -------------------- Add -------------------- #
+
 
 @bot.message_handler(commands=['add'])
 def add_table(message):
     """Add new table and name for chat.id."""
-    space_after_url_ind = 5 + message.text[5:].find(" ")
-    if space_after_url_ind == -1 or \
-       space_after_url_ind == len(message.text) or \
-       message.text[space_after_url_ind + 1] != '(' or message.text[-1] != ')':
-        bot.send_message(message.chat.id,
-                         "Please retry with syntax /add link\
-                         (name copied from table)")
-        return
-    export_url = get_export_url(message.text[5:space_after_url_ind])
-    if export_url[0] == 0:
-        bot.send_message(message.chat.id,
-                         "Please check your table link: " + export_url[1])
-        return
-    searching_name = message.text[space_after_url_ind + 1:]
-    insert_new_string(message.chat.id, export_url[1], searching_name)
-    bot.send_message(message.chat.id, "Ok for add person " + searching_name)
+    msg = bot.reply_to(message, """\
+        Вставьте ссылку с нужного листа google tables с публичным доступом
+        """)
+    user_table = Table()
+    bot.register_next_step_handler(msg, process_link_step, user_table)
 
+
+def process_link_step(message, user_table: Table):
+    if message.text in used_commands:
+        used_commands[message.text](message)
+        return
+    link = message.text
+    export_link = get_export_url(link)
+    if export_link[0] == 0:
+        msg = bot.reply_to(message, "Please insert valid google table url")
+        bot.register_next_step_handler(msg, process_link_step, user_table)
+        return
+    user_table.link = link
+    user_table.export_link = export_link[1]
+    msg = bot.reply_to(message, """\
+        Вставьте полностью скопированный текст ячейки,\
+            по которому можно отслеживать результаты
+        """)
+    bot.register_next_step_handler(msg, process_cell_step, user_table)
+
+
+def process_cell_step(message, user_table: Table):
+    if message.text in used_commands:
+        used_commands[message.text](message)
+        return
+    user_table.searching_cell = message.text
+    msg = bot.reply_to(message, """\
+        Название таблицы для вас (рекомендуется короткое)
+        """)
+    bot.register_next_step_handler(msg, process_name_step, user_table)
+
+
+def process_name_step(message, user_table: Table):
+    if message.text in used_commands:
+        used_commands[message.text](message)
+        return
+    user_table.name = message.text
+    insert_new_string(message.chat.id, user_table)
+    bot.send_message(message.chat.id, f"Ok for {user_table.searching_cell}")
+
+
+# -------------------- Show -------------------- #
 
 @bot.message_handler(commands=['show'])
 def show_result(message):
     """Show strings from tables."""
+    table_names = message.text.split(' ')[1:]
     search_requests = get_searching_requests(message.chat.id)
     if search_requests is None:
         bot.send_message(message.chat.id, "You did not add any table")
         return
     for request in search_requests:
-        table = get_table_from_pandas(request[0])
+        if request.name not in table_names:
+            continue
+        table = get_table_from_pandas(request.export_link)
         person_ind = -1
         for i in range(len(table)):
-            if table.iloc[i][0] == request[1] or \
-               table.iloc[i][1] == request[1]:
+            if request.searching_cell in [table.iloc[i][0], table.iloc[i][1]]:
                 person_ind = i
                 break
         if person_ind == -1:
@@ -116,8 +166,10 @@ def show_result(message):
         s_result = '\n'.join([str(table.columns[i]) + ': ' +
                              str(table.iloc[person_ind][i])
                              for i in range(table.shape[1])])
-        bot.send_message(message.chat.id, s_result)
+        bot.send_message(message.chat.id, f"{request.name}\n{s_result}")
 
+
+# -------------------- Clear -------------------- #
 
 @bot.message_handler(commands=['clear'])
 def clear_data(message):
@@ -126,4 +178,11 @@ def clear_data(message):
     bot.send_message(message.chat.id, "ok, deleted")
 
 
+used_commands = {
+    '/start': send_welcome,
+    '/help': send_welcome,
+    '/add': add_table,
+    '/show': show_result,
+    '/clear': clear_data,
+}
 bot.infinity_polling()
